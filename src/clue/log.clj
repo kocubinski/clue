c808e12147d6d95ab79332dc37876b61196d6cf5
Matt Kocubinski <mkocubinski@gmail.com>
2014-05-16 19:48:23 -0400

    Initial commit.

---
+++ src/clue/log.clj
@@ -0,0 +1,19 @@
+(ns clue.log
+  (:require [taoensso.timbre :as log]
+            [taoensso.timbre.appenders.rotor :as rotor]))
+
+(defonce log-config
+  (log/set-config!
+   [:appenders :rotor]
+   {:min-level :debug
+    :enabled? true
+    :async? false ; should be always false for rotor
+    :max-message-per-msecs nil
+    :fn rotor/appender-fn}))
+
+(defonce rotor-config
+  (log/set-config!
+    [:shared-appender-config :rotor]
+    {:path (str project-root "/log/practical.log") :max-size 1000000 :backlog 10}))
+
+
